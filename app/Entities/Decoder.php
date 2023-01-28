b58ab23b8d1c759fc2c3c0676fe6cfc555094f05
heintzel <heintzel.m@mpcreation.net>
2023-01-28 18:36:33 +0100

    style: remove var_dump

--- app/Entities/Decoder.php
+++ app/Entities/Decoder.php
@@ -7,7 +7,6 @@ class Decoder
     public function decode($message, Cipher $cipher): string
     {
         $messageArray = mb_str_split($message);
-        var_dump($messageArray);
 
         $decodedMessage = array_map(function ($element) use ($cipher) {
             return $cipher->decipherCharacter($element);
